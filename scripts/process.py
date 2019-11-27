ff3d1cf1031faedf73f0920cb5a4485cbcebc0d2
Carlos Sanchez <randomouscrap98@aol.com>
2019-11-27 22:16:17 +0000

    Initial messing around

---
+++ scripts/process.py
@@ -0,0 +1,20 @@
+# Carlos Sanchez
+# November, 2019
+# A script for periodically processing pokemon images
+
+import os
+
+# Tweak Config stuff
+MAXPROCESS=10
+
+# Probably won't change
+POKEAPI="https://pokeapi.co/api/v2/"
+LOCKFILE=".lock"
+
+# Magical functions!
+def Whatever():
+    print("WOW")
+
+# The main process loop.
+Whatever()
+
