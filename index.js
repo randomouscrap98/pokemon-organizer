3acf00c850b6ee6fa6e41201193b77c7dd2bfd28
Carlos Sanchez <randomouscrap98@aol.com>
2019-11-28 07:47:25 +0000

    Added form finder + tool area

--- index.js
+++ index.js
@@ -4,59 +4,76 @@ $(document).ready(function()
 
    $.ajaxSetup({ cache: false });
 
+   var findSubmit = $("#formfindersubmit");
+   findSubmit.click(function()
+   {
+      var species = $("#formfinderinput");
+      var url = encodeURI("https://pokeapi.co/api/v2/pokemon-species/" + species.val());
+      $.getJSON(url, function(data)
+      {
+         var forms = "Forms: \n";
+         for(var i = 0; i < data.varieties.length; i++)
+            forms += "\n" + data.varieties[i].pokemon.name;
+         alert(forms);
+      }).fail(function() { alert("Couldn't find pokemon"); });
+   });
+
    $.getJSON("data.json", function(data)
    {
       var header = $("h1");
       header.text(data.user + "'s " + header.text());
-      console.log(data);
 
       var pkmTable = $("#pokemon");
-
-      pkmTable.DataTable ({
-         "data" : data.list,
-         "pageLength" : 50,
-         "order" : [[ 2, "asc" ]],
-         "autoWidth" : true,
-         "columns" : [
-            { "data" : "thumb",
-              "orderable" : false,
-              "class" : "preview",
-              "render" : function(data, type, row, meta)
-              {
-                 var link = $("<a></a>");
-                 var image = $("<img>");
-                 link.attr("href", encodeURI(row.path));
-                 image.attr("src", encodeURI(data));
-                 link.append(image);
-                 return link[0].outerHTML;
-              }
-            },
-            { "data" : "name",
-              "class" : "name",
-              "render" : function(data, type, row, meta)
-              {
-                 var link = $("<a></a>");
-                 link.text(data);
-                 link.attr("target", "blank");
-                 link.attr("href", encodeURI(
-                    "https://www.serebii.net/pokemon/" + row["species"] + "/"));
-                 return link[0].outerHTML;
-              }
-            },
-            { "data" : "number",
-              "class" : "number"
-            },
-            { "data" : "generation",
-              "class" : "generation"
-            },
-            { "data" : "created",
-              "class" : "created",
-              "render" : function(data)
-              {
-                 return data.slice(0, 10);
-              }
-            },
-         ]
-      });
+      SetupPokeTable(pkmTable, data);
    });
 });
+
+function SetupPokeTable(pkmTable, data)
+{
+   pkmTable.DataTable ({
+      "data" : data.list,
+      "pageLength" : 50,
+      "order" : [[ 2, "asc" ]],
+      "autoWidth" : true,
+      "columns" : [
+         { "data" : "thumb",
+            "orderable" : false,
+            "class" : "preview",
+            "render" : function(data, type, row, meta)
+            {
+               var link = $("<a></a>");
+               var image = $("<img>");
+               link.attr("href", encodeURI(row.path));
+               image.attr("src", encodeURI(data));
+               link.append(image);
+               return link[0].outerHTML;
+            }
+         },
+         { "data" : "name",
+            "class" : "name",
+            "render" : function(data, type, row, meta)
+            {
+               var link = $("<a></a>");
+               link.text(data);
+               link.attr("target", "blank");
+               link.attr("href", encodeURI(
+               "https://www.serebii.net/pokemon/" + row["species"] + "/"));
+               return link[0].outerHTML;
+            }
+         },
+         { "data" : "number",
+            "class" : "number"
+         },
+         { "data" : "generation",
+            "class" : "generation"
+         },
+         { "data" : "created",
+            "class" : "created",
+            "render" : function(data)
+            {
+               return data.slice(0, 10);
+            }
+         },
+      ]
+   });
+}
